bae644786a129948341b47e00e49fa577b995740
xun ma <lazybone1995@hotmail.com>
2022-11-22 10:47:00 +0800

    set up article with activestorage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+i = 0
+5.times do
+  file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+  article = Article.new(title: "NES-#{i}", body: "A great console")
+  article.cover_image.attach(io: file, filename: "#{article.title.parameterize}.png", content_type: "image/png")
+  article.save
+  i += 1
+end
